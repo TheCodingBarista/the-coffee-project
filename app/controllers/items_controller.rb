6fce60c67571dbbce9558104a63b11d9deedf441
melissa_holt <melissa.holt21@gmail.com>
2020-03-17 17:02:06 -0700

    Building out items routes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,36 +2,48 @@ class ItemsController < ApplicationController
 
   # GET: /items
   get "/items" do
-    erb :"/items/index.html"
+    @items = Item.all
+    erb :"/items/index"
   end
 
   # GET: /items/new
   get "/items/new" do
-    erb :"/items/new.html"
+    erb :"/items/new"
   end
 
   # POST: /items
   post "/items" do
-    redirect "/items"
+    @item = Item.create(:name => params[:name], :category => params[:category], :quantity => params[:quantity], :store_id => params[:store_id])
+    redirect to "/items/#{item.id}"
   end
 
   # GET: /items/5
   get "/items/:id" do
-    erb :"/items/show.html"
+    @item = Item.find_by_id(params[:id])
+    erb :"/items/show"
   end
 
   # GET: /items/5/edit
   get "/items/:id/edit" do
-    erb :"/items/edit.html"
+    @item = Item.find_by_id(params[:id])
+    erb :"/items/edit"
   end
 
   # PATCH: /items/5
   patch "/items/:id" do
-    redirect "/items/:id"
+    @item = Item.find_by_id(params[:id])
+    @item.name = params[:name]
+    @item.category = params[:category]
+    @item.quantity = params[:quantity]
+    binding.pry
+    @item.save
+    redirect to "/items/#{@item.id}"
   end
 
   # DELETE: /items/5/delete
   delete "/items/:id/delete" do
+    @item = Item.find_by_id(params[:id])
+    @item.delete
     redirect "/items"
   end
 end
